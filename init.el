5ac8105075a5d8c4ed30c8f01ec0de26229e7bbd
Cal Paterson <cal@calpaterson.com>
2017-08-10 20:16:36 +0100

    Human readable dired sizes

--- init.el
+++ init.el
@@ -234,6 +234,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dired-listing-switches "-alh")
  '(global-flycheck-mode t nil (flycheck))
  '(helm-mode-fuzzy-match t)
  '(shell-file-name "/bin/sh")
