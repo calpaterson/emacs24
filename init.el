53e8c105d96daacb3f7eae50ff2a671f57a240b3
Cal Paterson <cal@calpaterson.com>
2018-01-20 17:32:26 +0000

    Update things that Custom added...

--- init.el
+++ init.el
@@ -238,3 +238,9 @@
  '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
 (put 'downcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
