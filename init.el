8dada28830270f616a768c074c892ad15fca4be3
Cal Paterson <cal@calpaterson.com>
2018-06-14 13:22:22 +0100

    Set a few more basic org settings

--- init.el
+++ init.el
@@ -232,6 +232,14 @@
 (when (fboundp 'electric-indent-mode)
   (electric-indent-mode -1))
 
+; Org
+(setq org-directory "~/Dropbox/documents/")
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 (require 'uniquify)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
